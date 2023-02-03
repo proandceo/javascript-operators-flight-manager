c787ef8fd3c00c47aadebeeb960eeff7b55d477a
proandceo <proandceo@gmail.com>
2023-02-03 16:02:16 +1000

    first

--- logic/flights.js
+++ logic/flights.js
@@ -1,4 +1,37 @@
 function Flights() {
+    function calculateNumberOfFlights(numberOfPassengers, flightCapacity){
+        if(numberOfPassengers <= 0 || !Number.isInteger(numberOfPassengers)){
+            throw new Error("The number of passengers must be a positive integer value");
+        }
 
+        if(flightCapacity <= 0 || !Number.isInteger(flightCapacity)){
+            throw new Error("The capacity of the flight must be a positive integer value");
+        }
+
+        let calculateNumberOfFlights = numberOfPassengers/flightCapacity;
+
+        if ( numberOfPassengers%flightCapacity != 0){
+            calculateNumberOfFlights++;
+        }
+        return Math.floor(calculateNumberOfFlights);
+    }
+
+    function checkAircraftRevision(distanceLimit, distancesArray){
+        let distanceSum = 0;
+        for (let i = 0; i < distancesArray.length; i++){
+            distanceSum += distancesArray[i];
+        }
+
+        if (distanceSum <= distanceLimit/2){
+            return "The revision needs to be done within the next 3 months";
+        } else if ( distanceSum <= distanceLimit/4*3) {
+            return "The revision needs to be done within the next 2 months";
+        } else if (distanceSum <= distanceLimit){
+            return "The revision needs to be done within the next month";
+        } else {
+            throw new Error("Error");
+        } 
+    }
+    return {calculateNumberOfFlights, checkAircraftRevision}; 
 }
-
+module.exports = Flights();
--- logic/passengers.js
+++ logic/passengers.js
@@ -1,4 +1,70 @@
 function Passengers() {
+    function checkFlightCapacity(flightCapacity, arrayOfPassengersNumbers){
+        let sumOfPassengersNumbers = 0
+        for (let i = 0; i < arrayOfPassengersNumbers.length; i++){
+            sumOfPassengersNumbers += arrayOfPassengersNumbers[i];
+        }
+        
+        if (sumOfPassengersNumbers < flightCapacity ){
+            return sumOfPassengersNumbers;
+        } else {
+            throw new Error("The capacity of the flight is exceeded");
+        }
+    }
 
+    function distributeAllSeatsToAllPassengers(vip, 
+                                            regular, 
+                                            numberOfFlights,
+                                            numberOfBusinessSeatsPerFlight, 
+                                            numberOfEconomySeatsPerFlight){
+        
+        distributeAllSeatsToAllPassengers.vipPassengersWithBusinessSeats = 0;
+        distributeAllSeatsToAllPassengers.vipPassengersWithEconomySeats = 0;
+        distributeAllSeatsToAllPassengers.regularPassengersWithBusinessSeats = 0;
+        distributeAllSeatsToAllPassengers.regularPassengersWithEconomySeats = 0;
+
+        if ( vip - numberOfFlights * numberOfBusinessSeatsPerFlight > 0){
+            distributeAllSeatsToAllPassengers.vipPassengersWithBusinessSeats = numberOfFlights * numberOfBusinessSeatsPerFlight;
+            distributeAllSeatsToAllPassengers.vipPassengersWithEconomySeats = vip - distributeAllSeatsToAllPassengers.vipPassengersWithBusinessSeats;  
+        }else {
+            distributeAllSeatsToAllPassengers.vipPassengersWithBusinessSeats = vip;
+            distributeAllSeatsToAllPassengers.vipPassengersWithBusinessSeats += numberOfFlights * numberOfBusinessSeatsPerFlight - vip;
+        }
+
+        if (numberOfFlights * numberOfEconomySeatsPerFlight - distributeAllSeatsToAllPassengers.vipPassengersWithEconomySeats > 0){
+            distributeAllSeatsToAllPassengers.regularPassengersWithEconomySeats = regular - 
+        }
+        
+
+        
+        // distributeAllSeatsToAllPassengers.vipPassengersWithBusinessSeats = vip;
+        // if (numberOfFlights * numberOfBusinessSeatsPerFlight - vip > 0){
+        //     distributeAllSeatsToAllPassengers.regularPassengersWithEconomySeats -= numberOfFlights * numberOfBusinessSeatsPerFlight - vip;
+        // }
+
+        // if ( vip > numberOfFlights * numberOfBusinessSeatsPerFlight){
+        //     distributeAllSeatsToAllPassengers.vipPassengersWithEconomySeats = (numberOfEconomySeatsPerFlight * numberOfFlights) - (numberOfFlights * numberOfBusinessSeatsPerFlight);
+        //     if (numberOfFlights * numberOfBusinessSeatsPerFlight - vip != 0){}
+        // }else {
+        //     distributeAllSeatsToAllPassengers.vipPassengersWithEconomySeats = 0;
+        // }
+        
+        // if(numberOfBusinessSeatsPerFlight * numberOfFlights - vip > 0){
+        //     distributeAllSeatsToAllPassengers.regularPassengersWithBusinessSeats = numberOfBusinessSeatsPerFlight*numberOfFlights - vip;
+        // } else {
+        //     distributeAllSeatsToAllPassengers.regularPassengersWithBusinessSeats = 0;
+        // }
+        // console.log("distributeAllSeatsToAllPassengers.regularPassengersWithEconomySeats " + distributeAllSeatsToAllPassengers.regularPassengersWithEconomySeats);
+
+
+        // console.log("numberOfEconomySeatsPerFlight * numberOfFlights " + numberOfEconomySeatsPerFlight * numberOfFlights);
+        // console.log("distributeAllSeatsToAllPassengers.vipPassengersWithEconomySeats " + distributeAllSeatsToAllPassengers.vipPassengersWithEconomySeats);
+        // console.log("distributeAllSeatsToAllPassengers.regularPassengersWithBusinessSeats " + distributeAllSeatsToAllPassengers.regularPassengersWithBusinessSeats);
+        // distributeAllSeatsToAllPassengers.regularPassengersWithEconomySeats = numberOfEconomySeatsPerFlight * numberOfFlights - (distributeAllSeatsToAllPassengers.vipPassengersWithEconomySeats + distributeAllSeatsToAllPassengers.regularPassengersWithBusinessSeats);
+
+
+        return distributeAllSeatsToAllPassengers;
+    }
+    return {checkFlightCapacity, distributeAllSeatsToAllPassengers};
 }
-
+module.exports = Passengers();
