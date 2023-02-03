7558cf3f2f00049d0ddc42ad85fb209cb21d9275
proandceo <proandceo@gmail.com>
2023-02-03 17:33:00 +1000

    Update price.js

--- logic/price.js
+++ logic/price.js
@@ -1,4 +1,22 @@
 function Prices() {
+    function calculateFinalPrice(basePrice, variationPassengerType, variationFlightType){
 
+        let sum = basePrice - (-variationPassengerType/100) *
+        basePrice - (-variationFlightType/100) * (basePrice - (-variationPassengerType/100) * basePrice);
+
+        return sum;
+    }
+
+    function checkInput(i){
+        if (i === ''){
+            throw "The input should not be empty";
+        }else if (Number.isInteger(i)){
+            throw "The input should be a number"
+        }
+
+        return 
+    }
+
+    return {calculateFinalPrice, checkInput};
 }
-
+module.exports = Prices();
